21f0cf6ce618e023fd9febf2232a5924a303e804
Jacob Sun <jacobsun000@gmail.com>
2023-10-14 15:10:54 -0500

    Update

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -168,7 +168,7 @@ return {
 
   -- formatters
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
     opts = function()
