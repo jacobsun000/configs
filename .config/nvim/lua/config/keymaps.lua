56a9afa739ae9efa90950675562397c851e58de6
Jacob Sun <jacobsun000@gmail.com>
2023-11-18 02:33:46 -0600

    feat(nvim): Robost keymap set function

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,17 @@
 -- Add any additional keymaps here
 
 local unmap = vim.keymap.del
-local map = vim.keymap.set
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
 -------- Leader Key  --------
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
