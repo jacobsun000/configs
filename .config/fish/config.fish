1a05f844358f1f2d10852a6afa75054eb33611cd
Jacob Sun <jacobsun000@gmail.com>
2023-11-21 15:09:27 -0600

    fix(fish): Lazygit config folder for MacOS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,6 +34,9 @@ set -x CMAKE_EXPORT_COMPILE_COMMANDS 1
 set MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set BAT_THEME "Monokai Extended Origin"
 
+# lazygit
+set XDG_CONFIG_HOME "$HOME/.config"
+
 # aliases
 alias cat=bat
 alias ls=exa
@@ -58,7 +61,7 @@ if test -f /home/jacob/miniconda3/bin/conda
     eval /home/jacob/miniconda3/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/home/jacob/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/jacob/miniconda3/etc/fish/conf.d/conda.fish"
+        ./home/jacob/miniconda3/etc/fish/conf.d/conda.fish
     else
         set -x PATH /home/jacob/miniconda3/bin $PATH
     end
