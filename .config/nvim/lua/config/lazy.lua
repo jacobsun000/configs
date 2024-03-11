14ae5d4a63814049b2257bc72a895523c4414ba2
Jacob Sun <jacobsun000@gmail.com>
2024-03-11 01:33:34 -0500

    feat(nvim): Latex support

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.tex" },
 
     -- import/override with your plugins
     { import = "plugins" },
