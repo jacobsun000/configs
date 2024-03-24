264f4c5b35dea00e4013f115f570139dfac29a50
Jacob Sun <jacobsun000@gmail.com>
2024-03-24 08:51:43 -0500

    feat(vim): Disable glsl_analyzer

--- .config/nvim/lua/plugins/glsl.lua
+++ .config/nvim/lua/plugins/glsl.lua
@@ -1,17 +1,18 @@
-return {
-  { "tikhomirov/vim-glsl", ft = "glsl" },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        glsl_analyzer = {},
-      },
-      setup = {
-        glsl_analyzer = function()
-          require("lspconfig").glsl_analyzer.setup({})
-          return true
-        end,
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   { "tikhomirov/vim-glsl", ft = "glsl" },
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         glsl_analyzer = {},
+--       },
+--       setup = {
+--         glsl_analyzer = function()
+--           require("lspconfig").glsl_analyzer.setup({})
+--           return true
+--         end,
+--       },
+--     },
+--   },
+-- }
