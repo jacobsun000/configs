ad1d9554ce2126e5b098b6ca5cf4427ea8dd6daa
Jacob Sun <jacobsun000@gmail.com>
2024-05-28 07:21:59 -0500

    fix(fish): Conda env variable update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -86,13 +86,13 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/jacob/miniconda3/bin/conda
-    eval /home/jacob/miniconda3/bin/conda "shell.fish" hook $argv | source
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" hook $argv | source
 else
-    if test -f "/home/jacob/miniconda3/etc/fish/conf.d/conda.fish"
-        ./home/jacob/miniconda3/etc/fish/conf.d/conda.fish
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        ./opt/miniconda3/etc/fish/conf.d/conda.fish
     else
-        set -x PATH /home/jacob/miniconda3/bin $PATH
+        set -x PATH /opt/miniconda3/bin $PATH
     end
 end
 # <<< conda initialize <<<
