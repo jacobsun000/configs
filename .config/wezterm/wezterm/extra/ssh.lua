d40757d576d4ac3a209fba7391931ef3aff87523
Jacob Sun <jacobsun000@gmail.com>
2024-06-02 16:26:41 -0500

    feat(wezterm): Config update

---
+++ .config/wezterm/wezterm/extra/ssh.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    -- This name identifies the domain
+    name = 'arch',
+    -- The hostname or address to connect to. Will be used to match settings
+    -- from your ssh config file
+    remote_address = 'arch',
+    -- The username to use on the remote host
+    username = 'jacob',
+    multiplexing = 'None',
+  },
+}
