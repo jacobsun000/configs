eaedca42ac943f7679587f05e924c467956d3ea5
Jacob Sun <jacobsun000@gmail.com>
2024-06-12 19:32:17 -0500

    feat(wezterm): Config update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,44 +1,44 @@
-local wezterm = require('wezterm')
-local ssh_domains = require('extra.ssh')
+local wezterm = require("wezterm")
+local ssh_domains = require("extra.ssh")
 
 local c = {}
 
 if wezterm.config_builder then
-  c = wezterm.config_builder()
+	c = wezterm.config_builder()
 end
 
 -- Appearance
-c.color_scheme = 'Monokai Pro (Gogh)';
-c.window_background_opacity = 0.85;
-c.hide_tab_bar_if_only_one_tab = true;
-c.use_fancy_tab_bar = true;
-c.text_background_opacity = 0.7;
-c.window_decorations = 'NONE';
+c.color_scheme = "Monokai Pro (Gogh)"
+c.window_background_opacity = 0.85
+c.hide_tab_bar_if_only_one_tab = true
+c.use_fancy_tab_bar = true
+c.text_background_opacity = 0.7
+c.window_decorations = "NONE"
+c.default_cursor_style = "SteadyBar"
+-- Font
+c.font = wezterm.font("SauceCodePro NF")
+c.font_size = 16
 
 -- Launching Programs
 c.wsl_domains = {
-  {
-    name = 'WSL:Arch',
-    distribution = 'Arch',
-    default_cwd = "/home/jacob",
-  }
+	{
+		name = "WSL:Arch",
+		distribution = "Arch",
+		default_cwd = "/home/jacob",
+	},
 }
-c.default_domain = 'WSL:Arch'
-
--- Font
-c.font = wezterm.font 'SauceCodePro NF';
-c.font_size = 16;
+c.default_domain = "WSL:Arch"
 
 -- Scroll bar
-c.enable_scroll_bar = true;
+c.enable_scroll_bar = true
 
 -- Key Bindings
--- disable_default_key_bindings = true;
+-- disable_default_key_bindings = true
 
-c.ssh_domains = ssh_domains;
+c.ssh_domains = ssh_domains
+c.window_close_confirmation = "NeverPrompt"
 
 -- Clipboard
-c.use_ime = true;
-c.default_gui_startup_args = { 'connect', 'unix' };
+c.use_ime = true
 
-return c;
+return c
