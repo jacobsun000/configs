493cc20be23c646775aa240546c0c13aa7531f95
Jacob Sun <jacobsun000@gmail.com>
2025-07-03 17:14:55 -0500

    fix(fish): PNPM path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -75,7 +75,7 @@ zoxide init fish | source
 direnv hook fish | source
 
 # pnpm
-set -gx PNPM_HOME "/home/jacob/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
